04c2d79618041523203669a9265c7f74eceeac0c
sensiz <dev.federicosensi@gmail.com>
2022-10-17 17:38:09 +0200

    create grid commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 // Consegna
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 
+function start () {
+
+    const rowEl = document.querySelector ('.row');
+
+    for (let i = 1; i <= 100; i++) {
+
+        const markUp = `<div class="col" src="" alt="">${i}</div>`
+        rowEl.insertAdjacentHTML('beforeend', markUp);
+    }
+
+
+
+}
+
+start();
 
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
